467eda68e857db31cdc2148abf7f751b77981498
Matt Bussing <mbussing@alarm.com>
2022-09-25 02:02:23 +0000

    got a test working

--- trip/main.rs
+++ trip/main.rs
@@ -58,7 +58,7 @@ fn main3(my_stdin: &mut impl BufRead, output: &mut impl Write) {
     output.write_all(buffer.as_bytes()).ok();
 }
 
-fn get_expected_amounts(mut divider: u32, mut total: f32, expected_amounts: &mut Vec<f32>) {
+fn get_expected_amounts(mut divider: u32, mut total: f32, expected_amounts: &mut Vec<f32>) -> f32 {
     while divider > 0 {
         let mut amount: f32 = total / divider as f32;
         // round to nearest two decimal places.
@@ -69,6 +69,8 @@ fn get_expected_amounts(mut divider: u32, mut total: f32, expected_amounts: &mut
         divider -= 1;
     }
     expected_amounts.sort_by(|a, b| a.partial_cmp(b).unwrap_or(Equal));
+
+    return total;
 }
 
 pub trait Round {
@@ -97,7 +99,11 @@ mod tests {
 
     use get_expected_amounts;
     use main3;
-    use std::str;
+    use std::{
+        env, fs, io,
+        path::{Path, PathBuf},
+        str,
+    };
     use Round;
 
     #[test]
@@ -204,90 +210,63 @@ mod tests {
     }
 
     #[test]
-    fn test_5() {
-        let mut output: Vec<u8> = Vec::new();
+    fn test_6() {
+        // Divide out the money.
+        // This will be a vector representing how much we give each person.
+        let mut expected_amounts: Vec<f32> = Vec::new();
+        let divider = 9;
+        let total: f32 = 10.;
+        let new_total = get_expected_amounts(divider, total, &mut expected_amounts);
 
-        main3(
-            // http://acm.student.cs.uwaterloo.ca/~acm00/990131.data/A.2.dat
-            &mut r"3
-10.00
-20.00
-30.00
-4
-15.00
-15.01
-3.00
-3.01
-6
-15.00
-15.00
-9.00
-9.00
-3.02
-3.01
-0"
-            .as_bytes(),
-            &mut output,
-        );
-        assert_eq!(str::from_utf8(&output).unwrap(), "$10.00\n$11.99\n$11.98\n");
+        let new_total2: f32 = expected_amounts.iter().sum();
+        assert_eq!(10.0, new_total2);
+
+        assert_eq!(true, new_total.abs() < 0.001, "{}", new_total.abs());
     }
 
-    //http://acm.student.cs.uwaterloo.ca/~acm00/990131.data/A.3.dat
     #[test]
-    fn test_5() {
-        let mut output: Vec<u8> = Vec::new();
+    fn test_7() {
+        // Divide out the money.
+        // This will be a vector representing how much we give each person.
+        let mut expected_amounts: Vec<f32> = Vec::new();
+        let divider = 2;
+        let total: f32 = 3.01;
+        let new_total = get_expected_amounts(divider, total, &mut expected_amounts);
 
-        main3(
-            &mut r"3
-10.00
-20.00
-30.00
-4
-15.00
-15.01
-3.00
-3.01
-6
-15.00
-15.00
-9.00
-9.00
-3.02
-3.01
-0"
-            .as_bytes(),
-            &mut output,
-        );
-        assert_eq!(str::from_utf8(&output).unwrap(), "$10.00\n$11.99\n$11.98\n");
+        let new_total2: f32 = expected_amounts.iter().sum();
+        assert_eq!(3.01, new_total2);
+
+        assert_eq!(true, new_total.abs() < 0.001, "{}", new_total.abs());
     }
 
-    // todo make sure total has 0
+    pub fn absolute_path(path: impl AsRef<Path>) -> io::Result<PathBuf> {
+        let path = path.as_ref();
+
+        let absolute_path = if path.is_absolute() {
+            path.to_path_buf()
+        } else {
+            env::current_dir()?.join(path)
+        };
+
+        Ok(absolute_path)
+    }
     #[test]
-    fn test_5() {
+    fn test_8() {
         let mut output: Vec<u8> = Vec::new();
 
-        main3(
-            &mut r"3
-10.00
-20.00
-30.00
-4
-15.00
-15.01
-3.00
-3.01
-6
-15.00
-15.00
-9.00
-9.00
-3.02
-3.01
-0"
-            .as_bytes(),
-            &mut output,
+        let path_string = String::from("./trip/data.dat");
+        let temp_path = absolute_path(Path::new(&path_string)).expect("absolute_path failed");
+        let path: &str = temp_path.to_str().expect("to_str failed");
+
+        let contents = fs::read_to_string(path)
+            .expect(&format!("Should have been able to read the file {}", path));
+
+        main3(&mut contents.as_bytes(), &mut output);
+        assert_eq!(
+            str::from_utf8(&output).unwrap(),
+            "$1214050.92\n$1284504.22\n$1268887.29\n$1262307.92\n$1237205.52\n$1281688.38\n$1234121.61\n$1241904.46\n$1290084.73\n$1234205.89"
         );
-        assert_eq!(str::from_utf8(&output).unwrap(), "$10.00\n$11.99\n$11.98\n");
     }
+
     // http://acm.student.cs.uwaterloo.ca/~acm00/990131.data/
 }
